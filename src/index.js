ee7d43441b62658e92a58f29da3f0e499f88ab4f
Bram Devries <bram@madewithlove.be>
2016-01-07 21:35:29 +0100

    Able to blacklist actions
    Signed-off-by: Bram Devries <bram@madewithlove.be>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-export default (socket) =>  {
-    return store => {
-        socket.on('redux-socket-middleware/DISPATCH_ACTION', (action) => {
-            store.dispatch({...action, dispatched: true});
-        });
+const defaultOptions = {
+  blacklist: [],
+};
 
-        return next => action => {
-            const result = next(action);
+export default (socket, options = defaultOptions) =>  {
+  options = Object.assign(defaultOptions, options);
 
-            if (!action.dispatched) {
-                socket.emit('redux-socket-middleware/RECEIVE_ACTION', action);
-            }
+  // Determines wether or not an action should be emitted to the socket server.
+  const shouldEmit = action => {
+    return !action.emitted // If the action was emitted before, we will not emit it again.
+        && !options.blacklist.includes(action.type); // Blacklisted actions are not emitted.
+  };
 
-            return result;
-        };
-    }
+  return store => {
+    // We listen to the DISPATCH_ACTION message of our socket server, which will trigger
+    // the given action on the client.
+    socket.on('redux-socket-middleware/DISPATCH_ACTION', (action) => {
+      store.dispatch({...action, emitted: true});
+    });
+
+    return next => action => {
+      // We first execute the action locally
+      const result = next(action);
+
+      // If the action should be emitted we do so.
+      if (shouldEmit(action)) {
+        socket.emit('redux-socket-middleware/RECEIVE_ACTION', action);
+      }
+
+      // Return the result to the next middleware.
+      return result;
+    };
+  }
 }
