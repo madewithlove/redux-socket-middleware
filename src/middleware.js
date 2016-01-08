ef3338ebdbeb827997ea4f4883d696754150a9a6
Bram Devries <bram@madewithlove.be>
2016-01-08 20:14:13 +0100

    Move socket setup method lower so it has access to the store
    Signed-off-by: Bram Devries <bram@madewithlove.be>

--- src/middleware.js
+++ src/middleware.js
@@ -17,47 +17,49 @@ export default function socketMiddleware(options = defaultOptions) {
         && options.actions.includes(action.type); // The action type needs to be included
   };
 
-  const setupSocket = (url) => {
-    socket = options.resolveSocket(url);
+  return store => {
+    const setupSocket = (url) => {
+      socket = options.resolveSocket(url);
 
-    socket.on(SOCKET_RECEIVE_ACTION, action => {
-      store.dispatch({...action, emitted: true});
-    });
-
-    socket.on('disconnect', () => {
-      store.dispatch({
-        type: SOCKET_DISCONNECT,
+      socket.on(SOCKET_RECEIVE_ACTION, action => {
+        store.dispatch({...action, emitted: true});
       });
-    });
 
-    socket.on('reconnect', () => {
-      store.dispatch({
-        type: SOCKET_CONNECT,
+      socket.on('disconnect', () => {
+        store.dispatch({
+          type: SOCKET_DISCONNECT,
+        });
       });
-    });
 
-    return socket;
+      socket.on('reconnect', () => {
+        store.dispatch({
+          type: SOCKET_CONNECT,
+        });
+      });
+
+      return socket;
+    }
+
+    return next => action => {
+      // We first execute the action locally
+      const result = next(action);
+
+      // We want to intercept a couple of actions related to sockets.
+      switch (action.type) {
+        case SOCKET_CONNECT:
+          if (!socket) {
+            socket = setupSocket(action.url);
+          }
+          break;
+      }
+
+      // If the action should be emitted we do so.
+      if (shouldEmit(action)) {
+        socket.emit(SOCKET_DISPATCH, action);
+      }
+
+      // Return the result to the next middleware.
+      return result;
+    };
   }
-
-  return store => next => action => {
-    // We first execute the action locally
-    const result = next(action);
-
-    // We want to intercept a couple of actions related to sockets.
-    switch (action.type) {
-      case SOCKET_CONNECT:
-        if (!socket) {
-          socket = setupSocket(action.url);
-        }
-        break;
-    }
-
-    // If the action should be emitted we do so.
-    if (shouldEmit(action)) {
-      socket.emit(SOCKET_DISPATCH, action);
-    }
-
-    // Return the result to the next middleware.
-    return result;
-  };
 }
